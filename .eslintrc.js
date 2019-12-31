131aff31c7b94a0f832886d38a7702bb92e80b9d
Shoaib Sharif <sharifshoaib@yahoo.com>
2019-12-30 18:48:02 -0800

    setup an express Api with babel eslint

---
+++ .eslintrc.js
@@ -0,0 +1,61 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  parser: "babel-eslint",
+  extends: [
+    "airbnb",
+    "prettier",
+    "prettier/react",
+    "plugin:import/errors",
+    "plugin:import/warnings"
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      modules: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/forbid-prop-types": [0, { forbid: ["any"] }],
+    "react/prop-types": 0,
+    "prettier/prettier": "error",
+    "spaced-comment": "off",
+    "no-console": "warn",
+    "consistent-return": "off",
+    "func-names": "off",
+    "object-shorthand": "off",
+    "no-process-exit": "off",
+    "no-param-reassign": "off",
+    "no-return-await": "off",
+    "no-underscore-dangle": "off",
+    "class-methods-use-this": "off",
+    "prefer-destructuring": ["error", { object: true, array: false }],
+    "no-unused-vars": ["error", { argsIgnorePattern: "req|res|next|val" }],
+    "import/export": 2,
+    "import/no-unresolved": [2, { commonjs: true, amd: true }],
+    "import/named": 2,
+    "import/namespace": 2,
+    "import/default": 2
+  },
+  env: {
+    jest: true,
+    browser: true,
+    node: true
+  },
+  settings: {
+    "import/resolver": {
+      "babel-module": {}
+    }
+  }
+};
